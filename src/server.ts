f8f91b2c79c0dfcaaeb024ba4ffe456e8427d69d
Onlyakr <onlyakr2477@gmail.com>
2026-03-08 16:20:53 +0700

    fix: update port in server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,14 @@ import router from "./routes/router";
 const app = express();
 
 // Take a port 3000 for running server.
-const port = process.env.PORT || 3000;
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // Handling '/' Request
 
 app.use(router);
 
 // Server setup
-app.listen(port, () => {
-	console.log(`TypeScript with Express 
+app.listen(port, "0.0.0.0", () => {
+  console.log(`TypeScript with Express
          http://localhost:${port}/`);
 });
